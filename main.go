268e15a16bcb7f07d48fdcb043439227150104fb
rodrigofrumento <rodrigo.frumento@gmail.com>
2023-01-02 22:05:55 -0300

    conexão banco e dinamico html

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func connDB() *sql.DB {
+	conn := "user=postgres dbname=go-loja password=postgres host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+type Produto struct {
+	Id         int
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -13,5 +33,34 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	db := connDB()
+
+	selectAllProds, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectAllProds.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectAllProds.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
